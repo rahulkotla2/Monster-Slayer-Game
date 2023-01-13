222837d338ab162b8117955b5682e89509636a92
rahulkotla2 <rahulkotla2@gmail.com>
2023-01-13 11:21:06 +0530

    Basic Features Initialisation

---
+++ app.js
@@ -0,0 +1,25 @@
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+    data() {
+        return {
+            monsterHealth: 100,
+            playerHealth: 100
+        }
+    },
+    methods: {
+        attackMonster() {
+            const attackValue = getRandomValue(5, 12);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        attackPlayer() {
+            const attackValue = getRandomValue(8, 15);
+            this.monsterHealth -= attackValue;
+        }
+    },
+})
+
+app.mount("#game");
